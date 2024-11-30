88d5fdfad3d81bf2bf9c83db35fdf85b9ed081a2
pedro <pedroramirezneira@gmail.com>
2024-11-30 18:24:50 -0300

    fix: timeslot id

--- util/database/database.ts
+++ util/database/database.ts
@@ -656,28 +656,31 @@ class Database {
                             )
                             for (const day of days) {
                                 const timeIds = await this.query<TimeID>(
-                                    "SELECT start_time_id, end_time_id FROM timeslot WHERE subject_id = ? AND day_id = ?",
+                                    "SELECT id, start_time_id, end_time_id FROM timeslot WHERE subject_id = ? AND day_id = ?",
                                     [subject.id, day.id]
                                 )
-                                if (timeIds.length != 0) {
-                                    day.timeslots = []
-                                    for (const timeId in timeIds) {
-                                        const startTime =
-                                            await this.query<Time>(
-                                                "SELECT * FROM time WHERE id = ?",
-                                                [timeIds[0].start_time_id]
-                                            )
-                                        delete startTime[0].id
-                                        const endTime = await this.query<Time>(
-                                            "SELECT * FROM time WHERE id = ?",
-                                            [timeIds[0].end_time_id]
-                                        )
-                                        delete endTime[0].id
-                                        day.timeslots.push({
-                                            start_time: startTime[0].time,
-                                            end_time: endTime[0].time,
-                                        })
-                                    }
+                                day.timeslots = []
+                                for (const _ in timeIds) {
+                                    const startTime = await this.query<Time>(
+                                        "SELECT * FROM time WHERE id = ?",
+                                        [timeIds[0].start_time_id]
+                                    )
+                                    delete startTime[0].id
+                                    const endTime = await this.query<Time>(
+                                        "SELECT * FROM time WHERE id = ?",
+                                        [timeIds[0].end_time_id]
+                                    )
+                                    delete endTime[0].id
+                                    console.log({
+                                        id: timeIds[0].id,
+                                        start_time: startTime[0].time,
+                                        end_time: endTime[0].time,
+                                    })
+                                    day.timeslots.push({
+                                        id: timeIds[0].id,
+                                        start_time: startTime[0].time,
+                                        end_time: endTime[0].time,
+                                    })
                                 }
                                 delete day.id
                                 subject.days.push(day)
@@ -691,7 +694,7 @@ class Database {
                     resolve({ error: rating.error })
                     return
                 }
-                result[0].rating = rating.result!.rating
+                result[0].rating = rating.result?.rating ?? 0.0
                 resolve({ result: result[0] })
             } catch (error) {
                 resolve({ error: (error as QueryError).errno })
@@ -978,7 +981,7 @@ class Database {
                     [`%${query}%`]
                 )
                 const result: Query = {
-                    subjects: subjects.length != 0 ? subjects : undefined,
+                    subjects: subjects,
                     teachers: teachers,
                 }
                 resolve({ result: result })
