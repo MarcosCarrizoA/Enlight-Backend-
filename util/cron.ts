c1d4a1f448ff045eccb155e259e06300bef5b3f9
Pedro Ramirez Neira <65603558+pedroramirezneira@users.noreply.github.com>
2024-12-02 11:16:35 -0300

    fix: update cron schedule with GMT 0

--- util/cron.ts
+++ util/cron.ts
@@ -3,7 +3,7 @@ import { sendReminders } from "./reminders"
 
 function startCron() {
     console.log("Cron job started")
-    cron.schedule("0 9 * * *", async () => {
+    cron.schedule("0 12 * * *", async () => {
         await sendReminders()
     })
 }
